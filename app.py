1001e0954134e9b5883abe0c2d48967d557b3f4f
Alfredokairos <raya.torpedo@gmail.com>
2026-02-21 19:42:53 -0700

    Update app layout and content

--- app.py
+++ app.py
@@ -6,8 +6,18 @@ st.set_page_config(
 )
 
 st.title("🌊 Marine Spatial Intelligence Portfolio")
-st.subheader("Luis Alfredo Carrillo Aguilar")
+st.subheader("M en C. Luis Alfredo Carrillo Aguilar")
+st.markdown("""
+correo: raya.torpedo@gmail.com
 
+celular: 6121072580
+""")
+with open("CV_Luis_Alfredo_Carrillo_2026.pdf", "rb") as file:
+    st.download_button(
+        label="Descargar CV",
+        data=file,
+        file_name="CV_Luis_Alfredo_Carrillo_2026.pdf",
+        mime="application/pdf")
 st.markdown("""
 Especialista en análisis espacial marino, fotogrametría 3D y modelado ambiental aplicado a conservación.
 
@@ -16,6 +26,7 @@ Este portafolio muestra herramientas interactivas desarrolladas para apoyar la t
 
 #st.image("D:/Users/User/Desktop/2026/Portafolio/assets/mapa_3d.png", use_container_width=True)
 st.video("assets/comparacion_mapas_final.mp4")
+st.markdown("Comparación de cobertura coralina entre diciembre 2023 y febrero 2024 en el mismo parche arrecifal.")
 
 st.markdown("---")
 st.markdown("Selecciona un proyecto en el menú lateral.")
